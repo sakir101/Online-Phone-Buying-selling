1ba659c51dc55c5c07bf52c8c7949ea3f762decc
sakir101 <sakirhossainf99@gmail.com>
2022-11-26 09:53:29 +0600

    payment

---
+++ src/Pages/Payment/CheckoutForm.js
@@ -0,0 +1,58 @@
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import React, { useEffect, useState } from 'react';
+
+const CheckoutForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (!stripe || !elements) {
+            return
+        }
+
+        const card = elements.getElement(CardElement);
+        if (card === null) {
+            return;
+        }
+
+    }
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
+                            },
+                        },
+                    }}
+                />
+                <button
+                    className='btn btn-sm mt-4 btn-primary'
+                    type="submit"
+                    disabled={!stripe || !clientSecret || processing}>
+                    Pay
+                </button>
+            </form>
+            <p className="text-red-500">{cardError}</p>
+            {
+                success && <div>
+                    <p className='text-green-500'>{success}</p>
+                    <p>Your transactionId: <span className='font-bold'>{transactionId}</span></p>
+                </div>
+            }
+        </>
+    );
+};
+
+export default CheckoutForm;
--- src/Pages/Payment/Payment.js
+++ src/Pages/Payment/Payment.js
@@ -1,12 +1,25 @@
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import CheckoutForm from './CheckoutForm';
 
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 const Payment = () => {
-    const {orgPrice, name} = useLoaderData();
+    const orderPay = useLoaderData();
+    const {orgPrice, name} = orderPay;
     return (
         <div>
             <h1 className='text-4xl font-bold text-center'>Payment</h1>
             <h2 className='text-2xl my-5'>You need to pay <strong className='text-red-500'>${orgPrice}</strong> for {name}</h2>
+            <div className='w-96 my-12'>
+                <Elements stripe={stripePromise}>
+                    <CheckoutForm
+                        orderPay={orderPay}
+                    />
+                </Elements>
+            </div>
         </div>
     );
 };
